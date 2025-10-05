7e07c04d6f4694a838c8a7fb20a002347e24a60e
Maria Mazza <mmazza@fsu.edu>
2025-10-05 11:46:23 -0700

    Do not require Higgs->bb decays

--- production/fragment.py
+++ production/fragment.py
@@ -4,6 +4,7 @@ externalLHEProducer = cms.EDProducer(
     "ExternalLHEProducer",
     args=cms.vstring("GRIDPACK_SED_PLACEHOLDER"),
     nEvents=cms.untracked.uint32(NEVENTS_SED_PLACEHOLDER),
+    generateConcurrently=cms.untracked.bool(False),
     numberOfParameters=cms.uint32(1),
     outputFile=cms.string("cmsgrid_final.lhe"),
     scriptName=cms.FileInPath("GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh")
@@ -14,7 +15,6 @@ from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
 generator = cms.EDFilter(
-    # "Pythia8HadronizerFilter",
     "Pythia8ConcurrentHadronizerFilter",                # for dipoleRecoil = on
     maxEventsToPrint=cms.untracked.int32(1),
     pythiaPylistVerbosity=cms.untracked.int32(1),
@@ -27,9 +27,6 @@ generator = cms.EDFilter(
         pythia8PSweightsSettingsBlock,
         processParameters = cms.vstring(
             "SpaceShower:dipoleRecoil = on",            # for dipoleRecoil = on
-            "25:m0 = 125.0",                            # for H-->bb decay
-            "25:onMode = off",                          # for H-->bb decay
-            "25:onIfAny = 5 -5",                        # for H-->bb decay
         ),
         parameterSets=cms.vstring(
             "pythia8CommonSettings",
